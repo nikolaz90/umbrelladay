ff850fa5e63f2d79184682bbac2a17e8fdfd01b0
nikolaz.90 <nikolazstacey@hotmail.co.uk>
2022-08-29 19:51:58 +0200

    minor details changed in about page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ function About() {
           Have little to no savings? Don't like thinking about it? Not sure you can ever save up for anything 'big'?
         </p>
         <p className='home-paragraph'>
-          Rainy days happen all the time, keep yourself and your family 'dry' by saving up for the things you will need.
+          Rainy days happen all the time, save up for the things you will need.
           Whether that is an emergency fund for those unforeseeable situations - like a car break down or redundancy. Or
           you would like to buy your first property, these calculators should give you a realistic breakdown of what monthly amount 
           you can put aside and how that can become a significant sum over time. 
@@ -21,35 +21,28 @@ function About() {
         </p>
         <p className='home-paragraph'>
           Play around with the simulators to help you understand what you are capable of achieving in savings. 
-          The simulators are designed to give you a rough idea of how much you can achieve, especially if you are consistant.
           Have a look at how interest rates can have an effect on the final goal over time. Although interest rates are actually
           really low at the moment, you may find some saving accounts are better than others and its worth shopping around. 
           Interest rates are currently changing at the moment and there is a lot of talk about inflation increasing. 
-          If you are unsure about what that means, its worth looking at the inflation calculator. It should demonstrate
-          how the value of money changes over time ... yes that's right, the value of money changes ... !
+          If you are unsure about what that means, its worth looking at the inflation calculator.
         </p>
         <h3>Why?</h3>
         <p className='home-paragraph'>
-            My name is Nikolaz and I have been making little calculators like the ones you can find on this website
-            as a hobby. But why!? Well, there is a lot of talk around inflation and wealth distribution at the moment, 
-            and the conversations are usually political, I think the result is that a lot of people 
+            I have been making little calculators like the ones you can find on this website
+            as a hobby. But why!? Well, there is a lot of talk around inflation at the moment, 
+            and the conversations can get political, I think the result is that a lot of people 
             disengage from that conversation.
         </p>
         <p className='home-paragraph'>
-            While it's tempting to think we might get lucky one day, some people are opting for high risk and high return 
-            investments. That is a personal choice and for some people, that is the right choice. 
+            While it's tempting to think we might get lucky one day and make a pot of money, some people are opting for high risk 
+            investments. That is a personal choice but shouldn't come at the cost of traditional saving methods. 
         </p>
         <p className='home-paragraph'>
             This website is not about giving advice as much as it is about demonstrating
-            that classic saving strategies are important, especially for those who haven't started yet. 
-            The problem is that personal finance, although it is affected by politics, 
-            is also a really important aspect of our lives. I think there are some people who avoid organising their personal
-            finances and that could potentially put them in a difficult position over time.
-            This could be because maybe some people don't know how to go about saving or because its not a school subject or they would 
-            rather just not think about it, because ... well perhaps they think its boring! I used to ... ! Woops!
+            that classic saving strategies are important, <em>especially</em> for those who haven't started yet. 
         </p>
         <p className='home-paragraph'> 
-            So if people, especially (but not only) the young, are getting themselves talking about and organising 
+            So if people, especially (but not only) the young, are getting themselves thinking about and organising 
             their personal finances, I am certain that they will be thanking themselves in the future. 
         </p>
     </section>
